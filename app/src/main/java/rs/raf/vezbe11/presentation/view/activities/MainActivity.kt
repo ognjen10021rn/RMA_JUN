418d4230bf2b3883f078edbe9436ffcf6b9f8b4f
Mladen <mpopovic9720rn@raf.rs>
2023-05-28 14:19:07 +0200

    init

---
+++ app/src/main/java/rs/raf/vezbe11/presentation/view/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package rs.raf.vezbe11.presentation.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import rs.raf.vezbe11.R
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
